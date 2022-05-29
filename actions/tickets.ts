2abbe7d2d132b2400288deb459c8ca3e3cf0422c
Zarko Jevtic <zarej@svrljig.net>
2022-05-29 10:01:50 +0200

    tenderly fixes

--- actions/tickets.ts
+++ actions/tickets.ts
@@ -4,34 +4,43 @@ import {
 	Event,
 	TransactionEvent,
 	WebhookEvent,
-} from '@tenderly/actions';
-import {ethers} from "ethers";
-
-import TicketNFT from '../frontend/src/artifacts/contracts/TicketNFT.sol/TicketNFT.json'
-
-export const ticketReservedFn: ActionFn = async (context: Context, event: Event) => {
+   } from '@tenderly/actions';
+   import { ethers } from "ethers";
+   
+   import TicketNFT from '../frontend/src/artifacts/contracts/TicketNFT.sol/TicketNFT.json'
+   
+   export const ticketReservedFn: ActionFn = async (context: Context, event: Event) => {
 	// let blockEvent = event as BlockEvent;
 	let txEvent = event as TransactionEvent;
 	console.log(txEvent);
 	let iface = new ethers.utils.Interface(TicketNFT.abi);
 	const result = iface.decodeEventLog("TicketReserved", txEvent.logs[0].data, txEvent.logs[0].topics);
 	console.log(result);
-}
-
-export const ticketSentFn: ActionFn = async (context: Context, event: Event) => {
+   }
+   
+   export const ticketSent123: ActionFn = async (context: Context, event: Event) => {
 	// let blockEvent = event as BlockEvent;
 	let txEvent = event as TransactionEvent;
 	console.log(txEvent);
 	let iface = new ethers.utils.Interface(TicketNFT.abi);
-	const result = iface.decodeEventLog("TicketSent", txEvent.logs[0].data, txEvent.logs[0].topics);
+	const result = iface.decodeEventLog("TicketSent", txEvent.logs[1].data, txEvent.logs[1].topics);
 	console.log(result);
-}
-
-export const trigerTicketSentFn: ActionFn = async (context: Context, event: Event) => {
+   }
+   
+   export const trigerTicketSentFn: ActionFn = async (context: Context, event: Event) => {
 	// let blockEvent = event as BlockEvent;
 	let txEvent = event as WebhookEvent;
 	console.log(txEvent);
-	let iface = new ethers.utils.Interface(TicketNFT.abi);
+	// let iface = new ethers.utils.Interface(TicketNFT.abi);
 	// const result = iface.decodeEventLog("TicketSent", txEvent.logs[0].data, txEvent.logs[0].topics);
 	// console.log(result);
-}
+   }
+
+// export const trigerTicketSentFn: ActionFn = async (context: Context, event: Event) => {
+// 	// let blockEvent = event as BlockEvent;
+// 	let txEvent = event as WebhookEvent;
+// 	console.log(txEvent);
+// 	let iface = new ethers.utils.Interface(TicketNFT.abi);
+// 	// const result = iface.decodeEventLog("TicketSent", txEvent.logs[0].data, txEvent.logs[0].topics);
+// 	// console.log(result);
+// }
